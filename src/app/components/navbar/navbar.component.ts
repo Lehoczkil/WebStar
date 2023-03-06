0699badd626f6a35c124b21f7a93dac8cbb2237c
lehoczkil <lehoczkilaszlo2002@gmail.com>
2023-03-06 11:44:58 +0100

    feat: Implement navbar component

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { LoginService } from "src/app/services/login.service";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.scss"],
 })
 export class NavbarComponent {
+  username = '';
+
+  constructor(private loginService: LoginService) {}
+
+  // Gets username
+  ngOnInit(): void {
+    const storedUser = localStorage.getItem('user');
+    const user = storedUser !== null ? JSON.parse(storedUser) : null;
+    this.username = user.firstName + ' ' + user.lastName;
+  }
+
+  // Logout the user, the service redirects to the login page
+  handleLogout() {
+    this.loginService.logout();
+  }
 
 }
