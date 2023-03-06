5e00bcd9b900dad30e7a93bb177b1e793b92d1d1
lehoczkil <lehoczkilaszlo2002@gmail.com>
2023-03-06 11:44:16 +0100

    feat: Implement auth guard for guarding pages needing authentication

---
+++ src/app/helpers/auth.guard.ts
@@ -0,0 +1,26 @@
+﻿import { Injectable } from "@angular/core";
+import {
+  Router,
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+} from "@angular/router";
+
+@Injectable({
+  providedIn: "root",
+})
+export class AuthGuard implements CanActivate {
+  constructor(public router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    const user = localStorage.getItem("user");
+    if (user) {
+      // Authorised so return true
+      return true;
+    }
+
+    // Not logged in so redirect to login page
+    this.router.navigate(["/login"]);
+    return false;
+  }
+}
