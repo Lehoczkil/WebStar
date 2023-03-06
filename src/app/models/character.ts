a5edcbbc10ca05798ce2eeec4fbbaf4f51a9db8a
lehoczkil <lehoczkilaszlo2002@gmail.com>
2023-03-06 11:48:18 +0100

    feat: Add model for characters coming from api response

---
+++ src/app/models/character.ts
@@ -0,0 +1,13 @@
+export class Character {
+  constructor(
+    public id: string,
+    public name: string,
+    public side: string,
+    public force: {
+      power: string;
+      midichlorian: number;
+    },
+    public createdTimestamp: number,
+    public description: string
+  ) {}
+}
